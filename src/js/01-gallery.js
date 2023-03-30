1fb2186af9ddba139b9aff1ef9ff1d5662e2b08d
Denis-Shupletsov <b128k2351763@gmail.com>
2023-03-30 19:13:16 +0300

    1 ok

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import "../css/common.css"
+import "../css/01-gallery.css"
 
-console.log(galleryItems);
+const createItemsMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+    </li>`;
+    }).join('');
+
+
+const containerEl = document.querySelector('.gallery');
+containerEl.insertAdjacentHTML('beforeend', createItemsMarkup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+    doubleTapZoom: 1,
+    scrollZoom: false,
+});
+containerEl.addEventListener('click', event => {
+    event.preventDefault();
+
+    const eTarget = event.target;
+    if (!eTarget.classList.contains('gallery__image')) {
+        return;
+    }
+});
+
+
+
+
+
